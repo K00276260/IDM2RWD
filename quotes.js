342de0a5cf43db6d730d7c4e01eb7718602c99ba
K00276260 <114913929+K00276260@users.noreply.github.com>
2022-10-19 09:52:46 +0100

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,85 @@
+localQuotes
+
+const localQuotes = [
+    {
+        text:"Innovation is taking two things that exist and putting them together in a new way.",
+        author:"Tom Freston"
+    },
+    {
+        text:"The value of an idea lies in the using of it.",
+        author:"Thomas Edison"
+    },
+    {
+        text:"What is the calculus of innovation? The calculus of innovation is really quite simple: Knowledge drives innovation, innovation drives productivity, productivity drives economic growth.",
+        author:"William Brody"
+    },
+    {
+        text:"Innovation is the unrelenting drive to break the status quo and develop anew where few have dared to go.",
+        author:"Steven Jeffes"
+    },
+    {
+        text:"Creativity is thinking up new things. Innovation is doing new things.",
+        author:"Theodore Levitt"
+    },
+    {
+        text:"If you look at history, innovation doesn’t come just from giving people incentives; it comes from creating environments where their ideas can connect.",
+        author:"Steven Johnson"
+    },
+    {
+        text:"If I had asked the public what they wanted, they would have said a faster horse.",
+        author:"Henry Ford"
+    },
+    {
+        text:"The secret of change is to focus all of your energy, not on fighting the old, but building on the new.",
+        author:"Socrates"
+    },
+    {
+        text:"When the winds of change blow, some people build walls and others build windmills.",
+        author:"Unknown"
+    },
+    {
+        text:"The best way to predict the future is to create it.",
+        author:"Alan Kay"
+    },
+    {
+        text:"You can’t allow tradition to get in the way of innovation. There’s a need to respect the past, but it’s a mistake to revere your past.",
+        author:"Bob Iger"
+    },
+    {
+        text:"The definition of insanity is doing the same thing over and over again, but expecting different results.",
+        author:"Albert Einstein"
+    },
+    {
+        text:"Don’t be afraid to take big steps when one is indicated. You can’t cross a chasm in two small jumps.",
+        author:"David Lloyd George"
+    },
+    {
+        text:"Be an innovator, not an imitator.",
+        author:"Audrey Carballo"
+    },
+    {
+        text:"There’s a way to do it better. Find it.",
+        author:"Thomas Edison"
+    },
+    {
+        text:"Innovation comes from saying NO to 1000 things.",
+        author:"Steve Jobs"
+    },
+    {
+        text:"Innovation survives only when people believe in their own ideas.",
+        author:"Levo League"
+    },
+    {
+        text:"If at first the idea is not absurd, then there is no hope for it.",
+        author:"Albert Einstein"
+    },
+    {
+        text:"Be less curious about people and more curious about ideas.",
+        author:"Marie Curie"
+    },
+    {
+        text:"If I have a thousand ideas and only one turns out to be good, I am satisfied.",
+        author:"Alfred Nobel"
+    },
+
+]
